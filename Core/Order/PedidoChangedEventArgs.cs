cada5fb8ccad6496a9296782ac7a65cbdde95901
Yamila <yamilaetchepareborda@hotmail.es>
2025-09-23 20:37:35 -0300

    Estructura base

---
+++ Core/Order/PedidoChangedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DeliveryGo.Core.Enum.Enums;
+
+namespace DeliveryGo.Core.Order
+{
+    public class PedidoChangedEventArgs : EventArgs
+    {
+        public int PedidoId { get; }
+        public EstadoPedido NuevoEstado { get; }
+        public DateTime Cuando { get; }
+
+        public PedidoChangedEventArgs(int id, EstadoPedido estado, DateTime cuando)
+            => (PedidoId, NuevoEstado, Cuando) = (id, estado, cuando);
+    }
+
+}
---
+++ Core/Order/PedidoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DeliveryGo.Core.Enum.Enums;
+
+namespace DeliveryGo.Core.Order
+{
+    public class PedidoService
+    {
+        public event EventHandler<PedidoChangedEventArgs>? EstadoCambiado;
+
+        public void CambiarEstado(int pedidoId, EstadoPedido nuevo)
+            => EstadoCambiado?.Invoke(this, new PedidoChangedEventArgs(pedidoId, nuevo, DateTime.Now));
+    }
+
+}
