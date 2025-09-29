c303d48c03d614ca6ef03efffaa5d1413709b33e
loloColombo <187066052+LoloColombo@users.noreply.github.com>
2025-09-29 19:42:24 -0300

    feat(et2): Implementación completa del servicio de envío y estrategias

--- Core/Strategy/Strategies/EnvioMoto.cs
+++ Core/Strategy/Strategies/EnvioMoto.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DeliveryGo.Interfaces;
 
 namespace DeliveryGo.Core.Strategy.Strategies
 {
-    internal class EnvioMoto
+    public class EnvioMoto : IEnvioStrategy
     {
+        public string Nombre => "Moto";
+
+        public decimal Calcular(decimal subtotal)
+        {
+            return 1200m;
+        }
     }
 }
