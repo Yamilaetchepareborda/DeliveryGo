fac08a1aff09d45cc80d6b3293ea050b3dd3763d
Manu Espinosa <emmaespinosa1010@gmail.com>
2025-10-03 14:56:17 -0300

    feat:Implemente carrito con command(undo/redo)

--- Core/Command/Item.cs
+++ Core/Command/Item.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace DeliveryGo.Core.Command
 {
-    internal class Item
+    //Clase que representa un item en el carrito de compras
+    public class Item
     {
+        //Sku: identificador unico del item
+
+        public string Sku { get; private set; } = "";
+        public string Nombre { get; private set; } = "";
+        public decimal Precio {get; private set; }
+        public int Cantidad {get; set;}
+
+        public Item(string sku, string nombre, decimal precio, int cantidad)
+        {
+            Sku = sku;
+            Nombre = nombre;
+            Precio = precio;
+            Cantidad = cantidad;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DeliveryGo.Core.Command;
+using DeliveryGo.Core.Command.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +9,11 @@ using System.Threading.Tasks;
 
 namespace DeliveryGo
 {
-    internal class Program
+    public class ProgramS
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
