4b6ede1c712abc381c7f5d522c5f6140f4ecb6e3
Iñaki Velo <velokako@gmail.com>
2025-10-05 23:00:40 -0300

    Etapa 3 final
    Etapa 3 definitiva

--- Core/Payment/Metodos/PagoMp.cs
+++ Core/Payment/Metodos/PagoMp.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DeliveryGo.Interfaces;
 
 namespace DeliveryGo.Core.Payment.Metodos
 {
-    internal class PagoMp
+    internal class PagoMp : IPago
     {
+        public string Nombre => "MercadoPago";
+
+        public bool Procesar(decimal monto)
+        {
+            if (monto <= 0m)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+
+
+
+        }
     }
 }
